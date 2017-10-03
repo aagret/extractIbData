e8e9d51df8ffab09a9c584393b36fd38abc1ef75
aagret <aagret@petray.ch>
2017-10-03 17:55:30 +0200

    new function to print stamp register

---
+++ printStamp.R
@@ -0,0 +1,20 @@
+
+## impression timbre
+
+setorder(timbre, OrderTime, Description, TradePrice, -ClientId)
+
+
+TF <- timbre
+
+
+# add opération Number
+trades <-  unique(timbre[, .(OrderTime, Description)])
+trades[, opNbr:= rownames(trades)]
+
+setkey(TF, OrderTime, Description)
+setkey(trades, OrderTime, Description)
+
+TF <- TF[trades]
+TF <- TF[, .(opNbr, TradeDate, Buy.Sell, Currency, abs(Quantity),
+             Description, TradePrice, ClientId,
+             SoumisSuisse, SoumisEtr, NonSoumis)]

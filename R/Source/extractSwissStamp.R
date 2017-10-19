ab7ec9406aad9392ec81795e8dd0ad9cc6f58d25
aagret <aagret@petray.ch>
2017-10-19 11:43:19 +0200

    remove )

--- R/Source/extractSwissStamp.R
+++ R/Source/extractSwissStamp.R
@@ -33,12 +33,12 @@ extractSwissStamp <- function(trade= ytdTrades) {
     setkey(db, OrderTime, Description, ClientId)
     
     # calc stamp amount
-    db[Currency ==  "CHF", ":=" (SoumisSuisse= Proceeds * Fx, 0),
+    db[Currency ==  "CHF", ":=" (SoumisSuisse= Proceeds * Fx,
                                  SoumisEtr= 0,
                                  NonSoumis= 0)]
     
     db[!Currency == "CHF", ":=" (SoumisSuisse= 0,
-                                 SoumisEtr= Proceeds * Fx, 0),
+                                 SoumisEtr= Proceeds * Fx,
                                  NonSoumis= 0)]
     
     

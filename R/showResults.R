2cf15fb870a56b0268bf7a04d30585a94c06866e
aagret <aagret@petray.ch>
2017-11-13 15:07:39 +0100

    add showResult tests

--- R/showResults.R
+++ R/showResults.R
@@ -6,42 +6,56 @@
 ############ TEST AND DEBUG ZONE
 ####
 
-# 
-# # group by quarter
-# ibClientNav[, Q:= quarter(TradeDate)]
-# 
-# ibClientNav[TradeDate > as.Date("2016-12-31") & 
-#               TradeDate <= as.Date("2017-09-29"), 
-#           .((adjNav[.N] - Hwm[1]) * perfFee / 100,
-#             (adjNav[1] + adjNav[.N]) /2 * advFee * 3 / 1200), 
-#           by= c("ClientId", "Q")]
-# 
-# 
-# 
-cli <- "U1427234" #"U2202020"  
+
+# group by quarter
+ibClientNav[, Q:= quarter(TradeDate)]
+
+ibClientNav[TradeDate > as.Date("2016-12-31") &
+              TradeDate <= as.Date("2017-12-31"),
+          .((adjNav[.N] - Hwm[1]) * perfFee / 100,
+            (adjNav[1] + adjNav[.N]) /2 * advFee * 3 / 1200),
+          by= c("ClientId", "Q")]
+
+
+library(quantmod)
+
+getSymbols("^GSPC", src="yahoo")
+SPX <- as.data.table(GSPC["2016-12-31/", 4])
+
+cli <- "U1427234" #"U2202020" 
+
 plot(ibClientNav[ClientId== cli,
                  .(TradeDate, exp(cumsum(c(0, ROC(Gross)[-1]))))], type="l", col="red")
 
 lines(ibClientNav[ClientId== cli,
                   .(TradeDate, exp(cumsum(c(0, ROC(fNet)[-1]))))], col="blue")
 
-# 
-# 
-# 
-# ibClientNav[, .(TradeDate, ibNav, adjNav, Gross, cNet,
-#               c(0, exp(cumsum(ROC(adjNav)[-1]))),
-#               c(0, exp(cumsum(ROC(Gross)[-1]))),
-#               c(0, exp(cumsum(ROC(cNet)[-1])))),
-#           by= ClientId][ , .SD[.N,], by= ClientId]
-# 
-# 
-# ibClientNav[, .SD[.N], by= ClientId,
-#           .SDcols= c("perfFee", "advFee", "Vat", "timbre")]
-# 
-# ibClientNav[TradeDate %in% as.Date(c("2016-12-30", "2017-03-31", "2017-06-30", "2017-09-29")), .SD, by= ClientId]
-# 
+
+lines(SPX[, .(index, exp(cumsum(c(0, ROC(GSPC.Close)[-1]))))])
+
+
+
+ibClientNav[, .(TradeDate, ibNav, adjNav, Gross, cNet,
+              c(0, exp(cumsum(ROC(adjNav)[-1]))),
+              c(0, exp(cumsum(ROC(Gross)[-1]))),
+              c(0, exp(cumsum(ROC(cNet)[-1])))),
+          by= ClientId][ , .SD[.N,], by= ClientId]
+
+
+ibClientNav[, .SD[.N], by= ClientId,
+          .SDcols= c("perfFee", "advFee", "Vat", "timbre")]
+
+ibClientNav[TradeDate %in% as.Date(c("2016-12-30", "2017-03-31", "2017-06-30", "2017-09-29")), .SD, by= ClientId]
+
+ibClientNav[,last(.SD,1), 
+            by=c("Q", "ClientId")]
+
+ibClientNav[,last(.SD,1), 
+            by=c("Q", "ClientId")][, sum(perfFee+advFee+Vat+timbre), by=c("Q", "ClientId")]
+
 
 ####
 ############ END OF TEST AND DEBUG ZONE
 ################################################################################
 ########################
+#
